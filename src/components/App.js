79bbb5ff0728cfc7ff77b145add6860de9794ec2
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-04-02 15:33:59 +0200

    ScrollToTop: add component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Router } from 'react-router-dom';
 import Resume from './Resume';
 import Content from './Main/Content';
 import NavBar from './Header/NavBar';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Content />} />
         <Route path="/my-resume" element={<Resume />} />
