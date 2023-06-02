ece3e24ee5fb00eee0213e505626adcde0f360a7
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-06-02 16:32:25 +0200

    add skills

--- src/data/data.js
+++ src/data/data.js
@@ -11,7 +11,16 @@ import psql from './../assets/image/stack/psql.png';
 import bootstrap from './../assets/image/stack/bootstrap.png';
 import git from './../assets/image/stack/git.png';
 import css from './../assets/image/stack/css.png';
+import sass from './../assets/image/stack/sass.png';
 import angular from './../assets/image/stack/angular.png';
+import linux from './../assets/image/stack/linux.png';
+import windows from './../assets/image/stack/windows.png';
+import prisma from './../assets/image/stack/prisma.svg';
+import nextjs from './../assets/image/stack/nextjs.png';
+import heroku from './../assets/image/stack/heroku.png';
+import jest from './../assets/image/stack/jest.png';
+
+import github from './../assets/image/github.png';
 
 
 const dataFrontend = 
@@ -77,6 +86,54 @@ const dataFrontend =
             "title": "Git",
             "image": git,
             "type": "devops"
+        },
+        {
+            "id": 11,
+            "title": "GitHub",
+            "image": github,
+            "type": "devops"
+        },
+        {
+            "id": 12,
+            "title": "Sass",
+            "image": sass,
+            "type": "frontend"
+        },
+        {
+            "id": 13,
+            "title": "Linux",
+            "image": linux,
+            "type": "devops"
+        },
+        {
+            "id": 14,
+            "title": "Windows",
+            "image": windows,
+            "type": "devops"
+        },
+        {
+            "id": 15,
+            "title": "NextJS",
+            "image": nextjs,
+            "type": "frontend"
+        },
+        {
+            "id": 16,
+            "title": "Prisma",
+            "image": prisma,
+            "type": "backend"
+        },
+        {
+            "id": 17,
+            "title": "Heroku",
+            "image": heroku,
+            "type": "devops"
+        },
+        {
+            "id": 18,
+            "title": "JEST",
+            "image": jest,
+            "type": "frontend"
         }
         
     ];
